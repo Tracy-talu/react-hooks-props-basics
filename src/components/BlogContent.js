ce97f246739d439b36a4352694edc45d808bb5b0
Tracy-talu <ttmtalu@gmail.com>
2023-07-26 16:37:21 +0300

    initial commit

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -2,6 +2,16 @@ import React from "react";
 
 function BlogContent(props) {
   console.log(props);
+  if (!props.isPublished){
+    return null;
+  }else{
+    return(
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    )
+  }
   
 }
 
